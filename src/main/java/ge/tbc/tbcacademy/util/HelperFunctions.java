594502a6dc45412d9fa4bf2f787e1d9209e6df30
Luka Tsinaridze <lukatsinaridze@gmail.com>
2024-04-25 12:54:11 +0400

    added hotfix

--- src/main/java/ge/tbc/tbcacademy/util/HelperFunctions.java
+++ src/main/java/ge/tbc/tbcacademy/util/HelperFunctions.java
@@ -2,6 +2,7 @@ package ge.tbc.tbcacademy.util;
 
 public class HelperFunctions {
     public static int getNumbersFromMixedString(String str) {
+        System.out.println("CONFLICT 4");
         return Integer.parseInt(str.replaceAll("[^\\d.%]", "").replace("%", ""));
     }
 }
